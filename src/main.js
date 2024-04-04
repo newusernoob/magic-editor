d70e362a5e6b0e83f3559d84231e1eddb899aba2
newusernoob <165213868+newusernoob@users.noreply.github.com>
2024-04-04 18:39:28 -0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-
-document.querySelector('#app').innerHTML = `
-<div>
-    <label for="i1">botão</label>
-    <input type="button" id="i1">
-    <input type="text" id="i2">
+let home = `
+<div class="auto">
+    <div class="center">
+        <label for="i1">botão</label>
+        <input type="button" id="i1">
+    </div>
 </div>
 `
+let projects = `
+<div class="auto">
+    <div class="center">
+        <div class="itens">
+            
+        </div>
+    </div>
+</div>
+`
+document.querySelector('#app').innerHTML = home
