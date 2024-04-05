db8f76fc456cbbbc684cc1f37de06ff83d425e66
newusernoob <165213868+newusernoob@users.noreply.github.com>
2024-04-05 14:34:56 -0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 var home = `<div class="auto"> <div class="center"> <label onclick="page(projects);">meus projetos</label> </div> </div>`;
-var projects = `<div class="auto"> <div class="center">  </div> <div class="center"><content style="opacity: 0;"> <input type="text"> <label style="background: rgb(95, 95, 255)" onclick="page(editor)">crir</label> </content> </div> </div>`;
+var configurations = ``;
+var projects = `<div class="auto"> <div class="center">  </div> <div class="center"><content style="disply: none;"> <input type="text"> <label style="background: rgb(95, 95, 255)" onclick="page(editor)">crir</label> </content> </div> </div>`;
 var editor = ``;
 
 function page(part){
