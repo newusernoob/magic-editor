6d40304cc1a5634c8a5f24d5fd7d70a9a82d92d2
newusernoob <165213868+newusernoob@users.noreply.github.com>
2024-04-05 14:20:57 -0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-var home = `<div class="auto"> <div class="center"> <label for="i1" onclick="">meus projetos</label> <input type="button"> </div> </div>`;var projects = ``;var editor = ``;
+var home = `<div class="auto"> <div class="center"> <label onclick="page(projects);">meus projetos</label> </div> </div>`;
+var projects = `<div class="auto"> <div class="center">  </div> <div class="center"> <content> <input type="text"> <label style="background: rgb(95, 95, 255)">crir</label> </content> </div> </div>`;
+var editor = ``;
 
 function page(part){
     document.querySelector('#app').innerHTML = part;
