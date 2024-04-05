29f274cce7d362dfd31bbcbdd438562ea0386146
newusernoob <165213868+newusernoob@users.noreply.github.com>
2024-04-05 15:07:57 -0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 var home = `<div class="auto"> <div class="center"> <label onclick="page(projects);">meus projetos</label> <label onclick="page(configurations);">configurações</label> </div> </div>`;
-var configurations = `<div class="auto"> <div class="center"> <input type="text" placeholder="cor de fundo"> <input type="text" placeholder=" cor de fonte"> <input type="text" placeholder="cor de caixa"> <br> <label onclick="localStorage.setiten('configurado', true);page(home)"></label> </div> </div>`;
+var configurations = `<div class="auto"> <div class="center"> <input type="text" placeholder="cor de fundo"> <input type="text" placeholder=" cor de fonte"> <input type="text" placeholder="cor de caixa"> <br> <label onclick="localStorage.setiten('configurado', true); page(home)"></label> </div> </div>`;
 var projects = `<div class="auto"> <div class="center"> <label class="box">+</label> </div> <div class="center"><content style="display: none;"> <input type="text"> <label style="background: rgb(95, 95, 255)" onclick="page(editor)">crir</label> </content> </div> </div>`;
-var editor = `<div>`;
+var editor = `<div class="auto"> <div class="right"> </div> </div>`;
 
 function page(part){
     document.querySelector('#app').innerHTML = part;
