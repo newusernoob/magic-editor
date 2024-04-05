7467035e4374258a74190136541149edd9258bd2
newusernoob <165213868+newusernoob@users.noreply.github.com>
2024-04-05 13:47:59 -0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 const home = `
 <div class="auto">
     <div class="center">
-        <label for="i1">botão</label>
+        <label for="i1">meus projetos</label>
         <input type="button" id="i1">
+        <label for=""></label>
+        <input id=">
     </div>
 </div>
 `
 const projects = `
 <div class="auto">
     <div class="center">
-        <label for="i1"><img src="img/icon.png"></img></label>
+        <label for="i1"" style="font-size:75px">+</label>
         <input type="button" id="i1">
     </div>
     <div class="box">
