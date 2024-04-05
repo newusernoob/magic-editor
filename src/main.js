3b18479a1d38b68c8bdbd402061ef70c9eb6fe53
newusernoob <165213868+newusernoob@users.noreply.github.com>
2024-04-05 14:09:52 -0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,35 +1,4 @@
-const home = `
-<div class="auto">
-    <div class="center">
-        <label for="i1" onclick="page(projects)">meus projetos</label>
-        <input type="button" id="i1">
-        <label for=""></label>
-        <input id=">
-    </div>
-</div>
-`
-const projects = `
-<div class="auto">
-    <div class="center">
-        <label for="i1"" style="font-size:75px" 
-        onclick="
-            page(projects);
-            document.querySelector('#box').style.opacity = 0;
-        ">+</label>
-        <input type="button" id="i1">
-    </div>
-    <div class="box" id="box">
-        <div class="center">
-            <input type="text" id="i2">
-        </div>
-    </div>
-</div>
-`
-const editor = `
-<div>
-    
-</div>
-`
+var home = `<div class="auto"> <div class="center"> <label for="i1" onclick="">meus projetos</label> <input type="button"> </div> </div>`;var projects = ``;var editor = ``;
 
 function page(part){
     document.querySelector('#app').innerHTML = part;
